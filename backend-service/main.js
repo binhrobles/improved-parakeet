a747c19f42eac54d26670a99087046cde0725d4f
Binh 🌱 <binhrobles@gmail.com>
2020-08-26 15:25:43 -0700

    do amazon transcribe formatting in the backend

--- backend-service/main.js
+++ backend-service/main.js
@@ -1,6 +1,31 @@
 const express = require('express')
 const http = require('http')
 const WebSocket = require('ws')
+const marshaller = require('@aws-sdk/eventstream-marshaller') // for converting binary event stream messages to and from JSON
+const util_utf8_node = require('@aws-sdk/util-utf8-node') // utilities for encoding and decoding UTF8
+
+// our converter between binary event streams messages and JSON
+const eventStreamMarshaller = new marshaller.EventStreamMarshaller(
+  util_utf8_node.toUtf8,
+  util_utf8_node.fromUtf8
+)
+
+// wrap the audio data in a JSON envelope
+const getAudioEventMessage = (buffer) => {
+  return {
+    headers: {
+      ':message-type': {
+        type: 'string',
+        value: 'event',
+      },
+      ':event-type': {
+        type: 'string',
+        value: 'AudioEvent',
+      },
+    },
+    body: buffer,
+  }
+}
 
 const app = express()
 
@@ -11,18 +36,19 @@ const server = http.createServer(app)
 const wss = new WebSocket.Server({ server })
 
 wss.on('connection', (ws) => {
-  //connection is up, let's add a simple simple event
-  ws.on('message', (message) => {
-    //log the received message and send it back to the client
-    console.log('received: %s', message)
-    ws.send(`Hello, you sent -> ${message}`)
-  })
+  ws.on('message', (chunk) => {
+    // add the right Transcribe JSON headers and structure to the chunk
+    const audioEventChunk = getAudioEventMessage(Buffer.from(chunk))
 
-  //send immediatly a feedback to the incoming connection
-  ws.send('Hi there, I am a WebSocket server')
+    //convert the JSON object + headers into a binary event stream chunk
+    const binary = eventStreamMarshaller.marshall(audioEventChunk)
+
+    console.log(audioEventChunk)
+    console.log(binary)
+  })
 })
 
 //start our server
 server.listen(8000, () => {
-  console.log(`Server started on port ${server.address().port} :)`)
+  console.log(`Server listening on port ${server.address().port}`)
 })
