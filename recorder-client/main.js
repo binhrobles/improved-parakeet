c18d1214dc8c19f5b2c5bbeea66765a0d91f6b19
Binh 🌱 <binhrobles@gmail.com>
2020-08-26 15:25:12 -0700

    do amazon transcribe formatting on server side

--- recorder-client/main.js
+++ recorder-client/main.js
@@ -1,49 +1,17 @@
-const marshaller = require('@aws-sdk/eventstream-marshaller') // for converting binary event stream messages to and from JSON
-const util_utf8_node = require('@aws-sdk/util-utf8-node') // utilities for encoding and decoding UTF8
 const mic = require('mic')
 const keypress = require('keypress')
 const WebSocket = require('ws')
 const audioUtils = require('./lib/audioUtils')
 
-// our converter between binary event streams messages and JSON
-const eventStreamMarshaller = new marshaller.EventStreamMarshaller(
-  util_utf8_node.toUtf8,
-  util_utf8_node.fromUtf8
-)
-
 const convertAudioToBinaryMessage = (audioChunk) => {
   if (audioChunk == null) return
 
   // downsample and convert the raw audio bytes to PCM
+  // TODO: can I not use sox to record audio in PCM format?
   const downsampledBuffer = audioUtils.downsampleBuffer(audioChunk)
   const pcmEncodedBuffer = audioUtils.pcmEncode(downsampledBuffer)
 
   return pcmEncodedBuffer
-
-  // add the right JSON headers and structure to the message
-  //const audioEventMessage = getAudioEventMessage(Buffer.from(pcmEncodedBuffer))
-
-  ////convert the JSON object + headers into a binary event stream message
-  //const binary = eventStreamMarshaller.marshall(audioEventMessage)
-
-  // return binary
-}
-
-const getAudioEventMessage = (buffer) => {
-  // wrap the audio data in a JSON envelope
-  return {
-    headers: {
-      ':message-type': {
-        type: 'string',
-        value: 'event',
-      },
-      ':event-type': {
-        type: 'string',
-        value: 'AudioEvent',
-      },
-    },
-    body: buffer,
-  }
 }
 
 // set up websocket connection
@@ -61,9 +29,7 @@ micInputStream.on('error', (err) => {
 ws.on('open', () => {
   micInputStream.on('data', (rawAudioChunk) => {
     // the audio stream is raw audio bytes. Transcribe expects PCM with additional metadata, encoded as binary
-    const binary = convertAudioToBinaryMessage(rawAudioChunk)
-
-    ws.send(binary)
+    ws.send(convertAudioToBinaryMessage(rawAudioChunk))
   })
 })
 
@@ -73,22 +39,25 @@ keypress(process.stdin)
 
 // listen for the "keypress" event
 process.stdin.on('keypress', (ch, key) => {
-  // space: toggle microphone
-  if (key && key.name == 'space') {
-    if (streaming) {
-      console.log('Stopping audio stream')
-      micInstance.stop()
-    } else {
-      console.log('Starting audio stream')
-      micInstance.start()
+  if (key) {
+    // space: toggle microphone
+    if (key.name == 'space') {
+      if (streaming) {
+        console.log('Stopping audio stream')
+        micInstance.stop()
+      } else {
+        console.log('Starting audio stream')
+        micInstance.start()
+      }
+      streaming = !streaming
     }
-    streaming = !streaming
-  }
 
-  // ctrl+c: exit the program
-  if (key && key.ctrl && key.name == 'c') {
-    process.stdin.pause()
-    ws.terminate()
+    // ctrl+c: exit the program
+    if (key.ctrl && key.name == 'c') {
+      console.log('Quitting')
+      process.stdin.pause()
+      ws.close()
+    }
   }
 })
 
