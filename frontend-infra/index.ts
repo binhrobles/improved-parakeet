ced9ac6e996bd10af2c8570e35bc92c2b5603ae2
Binh 🌱 <binhrobles@gmail.com>
2020-08-28 17:30:24 -0700

    add website hosting

--- frontend-infra/index.ts
+++ frontend-infra/index.ts
@@ -1,7 +1,29 @@
 import * as aws from '@pulumi/aws';
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket('transcribe-reader');
+const siteBucket = new aws.s3.Bucket('transcribe-reader', {
+  acl: 'public-read',
+  website: {
+    indexDocument: 'index.html',
+    errorDocument: 'index.html',
+  },
+});
+new aws.s3.BucketPolicy('bucketPolicy', {
+  bucket: siteBucket.bucket,
+  policy: siteBucket.bucket.apply((bucketName) =>
+    JSON.stringify({
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Effect: 'Allow',
+          Principal: '*',
+          Action: ['s3:GetObject'],
+          Resource: [`arn:aws:s3:::${bucketName}/*`],
+        },
+      ],
+    })
+  ),
+});
 
 // Export the name of the bucket
-export const bucketName = bucket.id;
+export const bucketName = siteBucket.id;
